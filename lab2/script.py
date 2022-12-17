6a50e9c6b17c684369776e8467b310cb1a8953e4
Даниил Петрайтис <dmpetraytis@edu.hse.ru>
2022-12-17 23:55:17 +0300

    lab2

---
+++ lab2/script.py
@@ -0,0 +1,31 @@
+import subprocess
+import sys
+
+
+def check_mtu(host, mtu):
+    request = ['ping', host, '-M', 'do', '-s', mtu, '-c', '1']
+    result = subprocess.run(request, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    return result
+
+
+def main(argv):
+    if len(argv) < 2:
+        raise "too many args"
+
+    host = argv[1]
+    l = 1
+    r = 2000
+
+    while r - l > 1:
+        mtu = (l + r) // 2
+        if check_mtu(host, str(mtu)):
+            l = mtu
+        else:
+            r = mtu
+        time.sleep(1)
+
+    print(f"MTU is {l}")
+
+
+if __name__ == '__main__':
+    main(sys.argv)
